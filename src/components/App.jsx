9484616bcee8e5ac3b8bcab6abe97501517446c6
Placebo98 <oleg.tsaryk98@gmail.com>
2023-09-22 02:19:52 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 
 import { ContactForm } from './Form/Form';
 import { Application } from './App.styled';
